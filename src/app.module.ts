4797ce62260f75cec096d9aa1def86b096449402
Clyzer <clyzer.dev@gmail.com>
2023-01-23 13:24:21 -0300

    Cambios finales a los Servicios/Modulos y Controladores

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,26 @@
 import { Module } from '@nestjs/common';
-import { SecurityController } from './controllers';
-import { AccountService } from './services';
+// Servicios
+import { SecurityService } from './services/security/security.service';
+import { AccountService } from './services/account/account.service';
+import { CustomerService } from './services/customer/customer.service';
+import { TransferService } from './services/transfer/transfer.service';
+import { DepositService } from './services/deposit/deposit.service';
+// Modulos
+import { SecurityModule } from './modules/security/security.module';
 import { AccountModule } from './modules/account/account.module';
 import { CustomerModule } from './modules/customer/customer.module';
-import { DepositModule } from './modules/deposit/deposit.module';
 import { TransferModule } from './modules/transfer/transfer.module';
+import { DepositModule } from './modules/deposit/deposit.module';
+// Controladores
+import { SecurityController } from './controllers/security/security.controller';
 import { AccountController } from './controllers/account/account.controller';
 import { CustomerController } from './controllers/customer/customer.controller';
-import { DepositController } from './controllers/deposit/deposit.controller';
 import { TransferController } from './controllers/transfer/transfer.controller';
+import { DepositController } from './controllers/deposit/deposit.controller';
 
 @Module({
-  imports: [AccountModule, CustomerModule, DepositModule, TransferModule],
+  imports: [SecurityModule, AccountModule, CustomerModule, DepositModule, TransferModule],
   controllers: [SecurityController, AccountController, CustomerController, DepositController, TransferController],
-  providers: [AccountService],
+  providers: [SecurityService, AccountService, CustomerService, TransferService, DepositService],
 })
 export class AppModule {}
