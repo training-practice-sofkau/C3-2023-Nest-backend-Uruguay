2b352b82418ab27dd7d95642f5b94e39b3584783
Xtian <xtiancuenta1@gmail.com>
2023-01-23 11:57:24 -0300

    Se hizo el servicio de customer

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { SecurityController } from './controllers';
 import { AccountService } from './services';
+import { CustomerService } from './services/customer/customer.service';
 
 @Module({
   imports: [],
   controllers: [SecurityController],
-  providers: [AccountService],
+  providers: [AccountService, CustomerService],
 })
 export class AppModule {}
