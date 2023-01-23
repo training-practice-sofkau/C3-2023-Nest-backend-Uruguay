87d85025b1622665366d857c0fdc34d75b844d9b
Xtian <xtiancuenta1@gmail.com>
2023-01-23 13:05:03 -0300

    Se importó security.controller en app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,11 @@ import { AccountTypeModule } from './modules/account_type/account_type.module';
 import { DocumentTypeModule } from './modules/document_type/document_type.module';
 import { TransferModule } from './modules/transfer/transfer.module';
 import { DepositModule } from './modules/deposit/deposit.module';
+import { SecurityController } from './controllers/security/security.controller';
 
 @Module({
   imports: [CustomerModule, AccountModule, AccountTypeModule, DocumentTypeModule, TransferModule, DepositModule],
-  controllers: [],
+  controllers: [SecurityController],
   providers: [],
 })
 export class AppModule {}
