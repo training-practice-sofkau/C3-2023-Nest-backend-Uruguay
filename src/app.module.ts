97f8d734b17bdb0e235fa0d751f0419b500fb322
Cristian <89285428+Cristiangonzz@users.noreply.github.com>
2023-01-23 13:46:06 -0300

    Quitando las interfaces que no estaban

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,14 @@ import { Module } from '@nestjs/common';
 import { SecurityController } from './controllers';
 import { AccountService } from './services';
 import { ModuleModule } from './module/module.module';
-import {  } from "module";
+import { AccountController } from "./controllers/account/account.controller";
+import { CusotmerController } from "./controllers/cusotmer/cusotmer.controller";
+import { DepositController } from "./controllers/deposit/deposit.controller";
+import { TansferController } from "./controllers/tansfer/tansfer.controller";
 
 @Module({
   imports: [ModuleModule],
-  controllers: [SecurityController],
+  controllers: [SecurityController,AccountController,CusotmerController,DepositController,TansferController],
   providers: [AccountService],
 })
 export class AppModule {}
