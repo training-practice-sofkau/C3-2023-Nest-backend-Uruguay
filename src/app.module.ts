eea8ec999d3ed9adce16c07630baaa1541436b2d
Cristian Tironi <tironi741@gmail.com>
2023-01-23 13:51:06 -0300

    Creamos los modulos de account, customer, deposit, transfer y security

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { SecurityController } from './controllers';
 import { AccountService } from './services';
+import { AccountModule } from './modules/account/account.module';
+import { CustomerModule } from './modules/customer/customer.module';
+import { TransferModule } from './modules/transfer/transfer.module';
+import { DepositModule } from './modules/deposit/deposit.module';
+import { SecurityModule } from './modules/security/security.module';
 
 @Module({
-  imports: [],
+  imports: [AccountModule, CustomerModule, TransferModule, DepositModule, SecurityModule],
   controllers: [SecurityController],
   providers: [AccountService],
 })
