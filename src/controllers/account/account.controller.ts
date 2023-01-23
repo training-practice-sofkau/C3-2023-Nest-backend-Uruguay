537edcd66b9a23988ff7a5d275473b04ce1a6bff
Clyzer <clyzer.dev@gmail.com>
2023-01-23 13:32:40 -0300

    Trabajo finalizado agregando interfaces

--- src/controllers/account/account.controller.ts
+++ src/controllers/account/account.controller.ts
@@ -1,4 +1,5 @@
 import { Controller } from '@nestjs/common';
+import { AccountType } from '../../interfaces/account_type/account_type.interface';
 
 @Controller('account')
 export class AccountController {}
--- src/controllers/customer/customer.controller.ts
+++ src/controllers/customer/customer.controller.ts
@@ -1,4 +1,5 @@
 import { Controller } from '@nestjs/common';
+import { DocumentType } from '../../interfaces/document_type/document_type.interface';
 
 @Controller('customer')
 export class CustomerController {}
