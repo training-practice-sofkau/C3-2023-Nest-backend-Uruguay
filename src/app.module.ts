ff391afc6c6fe83bc3b84259eeb2126d3d73a0d7
Rodrigo Caraballo <rodrigomcaraballo8@gmail.com>
2023-01-23 11:55:54 -0300

    Generando la estructura del Proyecto
    Agregando las carpetas y archivos faltantes dentro del proyecto

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SecurityController } from './controllers';
 import { AccountService } from './services';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [],
+  imports: [DatabaseModule],
   controllers: [SecurityController],
   providers: [AccountService],
 })
