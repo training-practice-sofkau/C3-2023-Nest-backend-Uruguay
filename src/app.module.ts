242965d651483935230a50d0256e4d4171fd441b
herweg <martindelgado2011@gmail.com>
2023-01-23 13:35:31 -0300

    Creacion de carpetas estructuradas

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
 import { SecurityController } from './controllers';
 import { AccountService } from './services';
+import { LoginModule } from './login/login.module';
+import { AccountModule } from './modules/account/account.module';
+import { TransferModule } from './modules/transfer/transfer.module';
+import { CustomerModule } from './modules/customer/customer.module';
+import { DepositModule } from './modules/deposit/deposit.module';
+import { CustomerService } from './customer/customer.service';
+import { TransferController } from './transfer/transfer.controller';
+import { DepositController } from './deposit/deposit.controller';
+import { AccountController } from './account/account.controller';
+import { CustomerController } from './customer/customer.controller';
+import { DepositService } from './deposit/deposit.service';
+import { TransferService } from './transfer/transfer.service';
+import { CustomerService } from './customer/customer.service';
 
 @Module({
-  imports: [],
-  controllers: [SecurityController],
-  providers: [AccountService],
+  imports: [LoginModule, AccountModule, TransferModule, DepositModule, CustomerModule],
+  controllers: [SecurityController, CustomerController, AccountController, DepositController, TransferController],
+  providers: [AccountService, CustomerService, DepositService, TransferService],
 })
 export class AppModule {}
