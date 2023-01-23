d4a8ca19cb999986e01717cbe8f937423ff9c0f4
Franco Torres <franco.torres1705@gmail.com>
2023-01-23 14:54:55 -0300

    agregando carpetas en dto y en entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,10 +12,15 @@ import { CustomerService } from './services/customer/customer.service';
 import { CustomerController } from './controllers/customer/customer.controller';
 import { TransferController } from './controllers/transfer/transfer.controller';
 import { DepositController } from './controllers/deposit/deposit.controller';
+import { DocumentTypeService } from './services/document_type/document_type.service';
+import { AccountTypeService } from './services/account_type/account_type.service';
+import { AccountTypeController } from './controllers/account_type/account_type.controller';
+import { DocumentTypeModule } from './modules/document_type/document_type.module';
+import { AccountTypeModule } from './modules/account_type/account_type.module';
 
 @Module({
-  imports: [TransferModule, DepositModule, AccountModule, SecurityModule, CustomerModule],
-  controllers: [SecurityController, CustomerController, TransferController, DepositController],
-  providers: [AccountService, DepositService, TransferService, CustomerService, ],
+  imports: [TransferModule, DepositModule, AccountModule, SecurityModule, CustomerModule, DocumentTypeModule, AccountTypeModule],
+  controllers: [SecurityController, CustomerController, TransferController, DepositController, AccountTypeController],
+  providers: [AccountService, DepositService, TransferService, CustomerService, DocumentTypeService, AccountTypeService, ],
 })
 export class AppModule {}
