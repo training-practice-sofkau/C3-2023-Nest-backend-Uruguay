4f5b40155552effa57272db993cdfffe4fd25932
Cristian Tironi <tironi741@gmail.com>
2023-01-23 14:01:20 -0300

    Creamos los services customer, deposit y transfer

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,10 +10,13 @@ import { AccountController } from './controllers/account/account.controller';
 import { CustomerController } from './controllers/customer/customer.controller';
 import { DepositController } from './controllers/deposit/deposit.controller';
 import { TransferController } from './controllers/transfer/transfer.controller';
+import { CustomerService } from './services/customer/customer.service';
+import { DepositService } from './services/deposit/deposit.service';
+import { TransferService } from './services/transfer/transfer.service';
 
 @Module({
   imports: [AccountModule, CustomerModule, TransferModule, DepositModule, SecurityModule],
   controllers: [SecurityController, AccountController, CustomerController, DepositController, TransferController],
-  providers: [AccountService],
+  providers: [AccountService, CustomerService, DepositService, TransferService],
 })
 export class AppModule {}
