7101e7012fa218564198d3c47dcaa1ff224f3af4
Franco Torres <franco.torres1705@gmail.com>
2023-01-23 13:50:59 -0300

    Agregando Controllers/Modules/Services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
 import { SecurityController } from './controllers';
 import { AccountService } from './services';
+import { DepositService } from './services/deposit/deposit.service';
+import { TransferService } from './services/transfer/transfer.service';
+import { TransferModule } from './modules/transfer/transfer.module';
+import { DepositModule } from './modules/deposit/deposit.module';
+import { AccountModule } from './modules/account/account.module';
+import { SecurityModule } from './modules/security/security.module';
+import { CustomerModule } from './modules/customer/customer.module';
+import { CustomerService } from './services/customer/customer.service';
+import { CustomerController } from './controllers/customer/customer.controller';
+import { TransferController } from './controllers/transfer/transfer.controller';
+import { DepositController } from './controllers/deposit/deposit.controller';
 
 @Module({
-  imports: [],
-  controllers: [SecurityController],
-  providers: [AccountService],
+  imports: [TransferModule, DepositModule, AccountModule, SecurityModule, CustomerModule],
+  controllers: [SecurityController, CustomerController, TransferController, DepositController],
+  providers: [AccountService, DepositService, TransferService, CustomerService, ],
 })
 export class AppModule {}
