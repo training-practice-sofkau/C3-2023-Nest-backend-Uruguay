aa51e71d1a23d3505a91b48ac10c2e88cdfb8bff
brunof <dlqq@outlook.es>
2023-01-23 14:58:44 -0300

    Segundo Commit Bruno Fernandez

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,10 +8,16 @@ import { DepositController } from './controllers/deposit/deposit.controller';
 import { DepositService } from './services/deposit/deposit.service';
 import { TransfersService } from './services/transfers/transfers.service';
 import { CustomerService } from './services/customer/customer.service';
+import { SecurityService } from './services/security/security.service';
+import { AccountModule } from './modules/account/account.module';
+import { CustomerModule } from './modules/customer/customer.module';
+import { DepositModule } from './modules/deposit/deposit.module';
+import { SecurityModule } from './modules/security/security.module';
+import { TransfersModule } from './modules/transfers/transfers.module';
 
 @Module({
-  imports: [],
+  imports: [AccountModule, CustomerModule, DepositModule, SecurityModule, TransfersModule],
   controllers: [SecurityController, AccountController, CustomerController, TransfersController, DepositController],
-  providers: [AccountService, DepositService, TransfersService, CustomerService],
+  providers: [AccountService, DepositService, TransfersService, CustomerService, SecurityService],
 })
 export class AppModule {}
