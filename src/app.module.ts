00f2ab559d1c8eb38cd7c83c95862288fe59efad
brunof <dlqq@outlook.es>
2023-01-23 14:32:38 -0300

    Primer Commit Bruno Fernandez

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { SecurityController } from './controllers';
 import { AccountService } from './services';
+import { AccountController } from './controllers/account/account.controller';
+import { CustomerController } from './controllers/customer/customer.controller';
+import { TransfersController } from './controllers/transfers/transfers.controller';
+import { DepositController } from './controllers/deposit/deposit.controller';
+import { DepositService } from './services/deposit/deposit.service';
+import { TransfersService } from './services/transfers/transfers.service';
+import { CustomerService } from './services/customer/customer.service';
 
 @Module({
   imports: [],
-  controllers: [SecurityController],
-  providers: [AccountService],
+  controllers: [SecurityController, AccountController, CustomerController, TransfersController, DepositController],
+  providers: [AccountService, DepositService, TransfersService, CustomerService],
 })
 export class AppModule {}
