16807a402019e9c6732fbf00a90af5ec5ce613de
Clyzer <clyzer.dev@gmail.com>
2023-01-23 12:57:31 -0300

    Agregada la estructura estandar y agregado los modelos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { SecurityController } from './controllers';
 import { AccountService } from './services';
+import { AccountModule } from './modules/account/account.module';
+import { CustomerModule } from './modules/customer/customer.module';
+import { DepositModule } from './modules/deposit/deposit.module';
+import { TransferModule } from './modules/transfer/transfer.module';
+import { AccountController } from './controllers/account/account.controller';
+import { CustomerController } from './controllers/customer/customer.controller';
+import { DepositController } from './controllers/deposit/deposit.controller';
+import { TransferController } from './controllers/transfer/transfer.controller';
 
 @Module({
-  imports: [],
-  controllers: [SecurityController],
+  imports: [AccountModule, CustomerModule, DepositModule, TransferModule],
+  controllers: [SecurityController, AccountController, CustomerController, DepositController, TransferController],
   providers: [AccountService],
 })
 export class AppModule {}
