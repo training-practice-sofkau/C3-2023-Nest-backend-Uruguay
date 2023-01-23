9e75dd11c0d06f9f281b0ba5524e3fdc3e71d699
Xtian <xtiancuenta1@gmail.com>
2023-01-23 12:25:47 -0300

    Se creó el resto de servicios y las carpetas mostradas en la master class

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { SecurityController } from './controllers';
 import { AccountService } from './services';
 import { CustomerService } from './services/customer/customer.service';
+import { AccountTypeService } from './services/account_type/account_type.service';
+import { DocumentTypeService } from './services/document_type/document_type.service';
+import { TransferService } from './services/transfer/transfer.service';
+import { DepositService } from './services/deposit/deposit.service';
 
 @Module({
   imports: [],
   controllers: [SecurityController],
-  providers: [AccountService, CustomerService],
+  providers: [AccountService, CustomerService, AccountTypeService, DocumentTypeService, TransferService, DepositService],
 })
 export class AppModule {}
