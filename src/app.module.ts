84e01c820c5cf770c0851d3c2943995ea477d217
Edinson Acosta Gancharov <acostaedinson11@gmail.com>
2023-01-23 13:40:31 -0300

    modules added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { SecurityController } from './controllers';
 import { AccountService } from './services';
-
+import { DocumentTypeModule } from './modules/document-type/document-type.module';
+import { AccountTypeModule } from './modules/account-type/account-type.module';
+import { AccountModule } from './modules/account/account.module';
+import { CostumerModule } from './modules/costumer/costumer.module';
+import { TransferModule } from './modules/transfer/transfer.module';
+import { DepositModule } from './modules/deposit/deposit.module';
+import { CustomerModule } from './modules/customer/customer.module';
 @Module({
-  imports: [],
+  imports: [DocumentTypeModule, AccountTypeModule, AccountModule, CostumerModule, TransferModule, DepositModule, CustomerModule],
   controllers: [SecurityController],
   providers: [AccountService],
 })
