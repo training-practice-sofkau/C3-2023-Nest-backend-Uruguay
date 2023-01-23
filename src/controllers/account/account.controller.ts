ddefcf3004e08fb4a4d6072c4dce0552fde794d6
Clyzer <clyzer.dev@gmail.com>
2023-01-23 14:10:12 -0300

    Trabajo finalizado, agregado de modulos, controladores y servicios faltantes

--- src/controllers/account/account.controller.ts
+++ src/controllers/account/account.controller.ts
@@ -1,5 +1,4 @@
 import { Controller } from '@nestjs/common';
-import { AccountType } from '../../interfaces/account/account_type/account_type.interface';
 
 @Controller('account')
 export class AccountController {}
--- src/controllers/customer/customer.controller.ts
+++ src/controllers/customer/customer.controller.ts
@@ -1,5 +1,4 @@
 import { Controller } from '@nestjs/common';
-import { DocumentType } from '../../interfaces/customer/document_type/document_type.interface';
 
 @Controller('customer')
 export class CustomerController {}
