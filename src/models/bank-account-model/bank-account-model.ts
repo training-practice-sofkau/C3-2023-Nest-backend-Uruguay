c445fbfdc67006fa5d0ca03939a83748a53176d9
brunof <dlqq@outlook.es>
2023-01-24 15:40:01 -0300

    Implementacion Clase Abstracta

--- src/models/bank-account-model/bank-account-model.ts
+++ src/models/bank-account-model/bank-account-model.ts
@@ -1,1 +1,17 @@
-export class BankAccountModel {}
+export abstract class BankAccountModel {
+
+    
+    acc_id : number;
+    customer_id : number;
+    type_Account_id : number;
+    balance: number;
+    daletedAt?: Date | number ;
+    
+    
+    algunMetodoAbstracto() {
+        throw new Error("Esto es un metodo abstracto");
+    }
+
+
+
+}
--- src/models/type-bank-account-model/type-bank-account-model.ts
+++ src/models/type-bank-account-model/type-bank-account-model.ts
@@ -1,1 +1,5 @@
-export class TypeBankAccountModel {}
+export class TypeBankAccountModel {
+
+
+    
+}
