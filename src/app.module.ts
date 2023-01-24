8d47622489981991f684ed67de454d90cadc5721
federicogandaria <federicogandaria@gmail.com>
2023-01-24 11:53:52 -0300

    Agregando archivos nuevos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SecurityController } from './controllers';
 import { AccountService } from './services';
-import { AccountController } from './module/account/account.controller';
-import { TransfersController } from './controllers/transfers/transfers.controller';
-import { CustomerController } from './controllers/customer/customer.controller';
-import { DepositController } from './controllers/deposit/deposit.controller';
-import { AccountService } from './module/account/account.service';
-import { CustomerService } from './services/customer/customer.service';
-import { DepositService } from './services/deposit/deposit.service';
-import { TransfersService } from './services/transfers/transfers.service';
-import { AccountModule } from './module/account/account.module';
-import { CustomerModule } from './module/customer/customer.module';
-import { TransfersModule } from './module/transfers/transfers.module';
-import { DepositModule } from './module/deposit/deposit.module';
-import { SecurityModule } from './module/security/security.module';
-import { DatabaseModule } from './database/database.module';
-import { ModuleModule } from './module/module.module';
 
 @Module({
-  imports: [AccountModule, CustomerModule, TransfersModule, DepositModule, SecurityModule, DatabaseModule, ModuleModule],
-  controllers: [SecurityController, AccountController, TransfersController, CustomerController, DepositController],
-  providers: [AccountService, CustomerService, DepositService, TransfersService],
+  imports: [],
+  controllers: [SecurityController],
+  providers: [AccountService],
 })
 export class AppModule {}
---
+++ src/persistence/entities/index.ts
@@ -0,0 +1,2 @@
+export * from './customer.entity';
+export * from './document-type.entity';
