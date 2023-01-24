4d8c5e897f7128f3615d5355882addddaeb9fd35
Rodrigo Caraballo <rodrigomcaraballo8@gmail.com>
2023-01-24 12:39:24 -0300

    Little changes in repo
    Making intial changes in repositories

--- src/persistence/repositories/account.repository.ts
+++ src/persistence/repositories/account.repository.ts
@@ -1,1 +1,21 @@
-export class AccountRepository {}
+import { CustomerEntity } from '../entities/customer.entity';
+import { CRUDRepo } from './interfaces/CRUD.interface';
+import { AccountEntity } from '../entities/account.entity';
+export class AccountRepository implements CRUDRepo {
+    register(entity: AccountEntity): AccountEntity {
+        throw new Error('Method not implemented.');
+    }
+    update(entity: AccountEntity): AccountEntity {
+        throw new Error('Method not implemented.');
+    }
+    delete(entity: AccountEntity): void {
+        throw new Error('Method not implemented.');
+    }
+    findAll(): AccountEntity[] {
+        throw new Error('Method not implemented.');
+    }
+    findById(id: string): AccountEntity {
+        throw new Error('Method not implemented.');
+    }
+
+}
--- src/persistence/repositories/customer.repository.ts
+++ src/persistence/repositories/customer.repository.ts
@@ -1,1 +1,28 @@
-export class CustomerRepository {}
+import { Injectable } from '@nestjs/common';
+import { CustomerEntity } from '../entities/customer.entity';
+import { CRUDRepo } from './interfaces/CRUD.interface';
+@Injectable()
+export class CustomerRepository implements CRUDRepo{
+    private readonly database: Array<CustomerEntity>;
+
+    constructor() {
+        this.database = new Array<CustomerEntity>;
+    }
+    register(entity: CustomerEntity): CustomerEntity {
+        throw new Error('Method not implemented.');
+    }
+    update(entity: CustomerEntity): CustomerEntity {
+        throw new Error('Method not implemented.');
+    }
+    delete(entity: CustomerEntity): void {
+        throw new Error('Method not implemented.');
+    }
+    findAll(): CustomerEntity[] {
+        throw new Error('Method not implemented.');
+    }
+    findById(id: string): CustomerEntity {
+        throw new Error('Method not implemented.');
+    }
+
+
+}
