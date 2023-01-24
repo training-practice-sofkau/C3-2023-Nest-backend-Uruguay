1a8809916a2957dfd22e8bae90d87b87789afc6c
Edinson Acosta Gancharov <acostaedinson11@gmail.com>
2023-01-23 21:20:31 -0300

    Minor fixes in app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,14 @@
 import { Module } from '@nestjs/common';
 import { SecurityController } from './controllers';
-import { AccountModule } from './modules/account/account.module';
-import { TransferModule } from './modules/transfer/transfer.module';
-import { DepositModule } from './modules/deposit/deposit.module';
-import { CustomerModule } from './modules/customer/customer.module';
 import { AccountController } from './controllers/account/account.controller';
 import { DepositController } from './controllers/deposit/deposit.controller';
 import { TransferController } from './controllers/transfer/transfer.controller';
 import { CustomerController } from './controllers/customer/customer.controller';
 import { DepositService } from './services/deposit/deposit.service';
 import { CustomerService } from './services/customer/customer.service';
+
 @Module({
-  imports: [AccountModule, TransferModule, DepositModule, CustomerModule],
+  imports: [],
   controllers: [
     SecurityController,
     AccountController,
