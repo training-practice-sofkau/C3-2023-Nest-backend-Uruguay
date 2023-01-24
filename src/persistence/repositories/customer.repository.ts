586710476dcf8a25472c98d9bcf269af58f883f5
Clyzer <clyzer.dev@gmail.com>
2023-01-24 14:34:42 -0300

    Added repositorys to project

--- src/persistence/repositories/customer.repository.ts
+++ src/persistence/repositories/customer.repository.ts
@@ -1,31 +1,21 @@
 import { Injectable } from '@nestjs/common';
-import { CustomerEntity } from '../entities';
+import { CustomerEntity, DocumentTypeEntity } from '../entities';
+import { GeneralCRUD } from './base/GeneralCRUD.base';
 
 @Injectable()
-export class CustomerRepository {
+export class CustomerRepository extends GeneralCRUD implements CustomerEntity {
   private readonly database: Array<CustomerEntity>;
+  documentType: DocumentTypeEntity;
+  document: string;
+  fullName: string;
+  email: string;
+  phone: string;
+  password: string;
+  avatarUrl: string | null;
+  state: boolean;
 
   constructor() {
+    super();
     this.database = new Array<CustomerEntity>();
-  }
-
-  register(entity: CustomerEntity): CustomerEntity {
-    throw new Error('This method is not implemented');
-  }
-
-  update(id: string, entity: CustomerEntity): CustomerEntity {
-    throw new Error('This method is not implemented');
-  }
-
-  delete(id: string, soft?: boolean): void {
-    throw new Error('This method is not implemented');
-  }
-
-  findAll(): CustomerEntity[] {
-    throw new Error('This method is not implemented');
-  }
-
-  findOneById(id: string): CustomerEntity {
-    throw new Error('This method is not implemented');
   }
 }
