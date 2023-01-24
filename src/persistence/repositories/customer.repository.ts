462fd0e0e44b9c73e9c4b66960c0397a99a79b78
Cristian <89285428+Cristiangonzz@users.noreply.github.com>
2023-01-24 16:13:06 -0300

    Title: Taller 2
    Descripcion: Creando una clase abstracta y extendiendola a la clase DocumentTypeRepository y CustomerRepository

--- src/persistence/repositories/customer.repository.ts
+++ src/persistence/repositories/customer.repository.ts
@@ -1,32 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { CustomerEntity } from '../entities';
-import { IRepositoriesGlobal } from './interfaces/repositories-global.interface';
+import { Repository } from './base/Repository';
+
 
 @Injectable()
-export class CustomerRepository implements IRepositoriesGlobal<CustomerEntity>  {
-  private readonly database: Array<CustomerEntity>;// coleccion de clientes 
-
-  constructor() {
-    this.database = new Array<CustomerEntity>();
-  }
-
-  register(entity: CustomerEntity): CustomerEntity {
-    throw new Error('This method is not implemented');
-  }
-
-  update(id: string, entity: CustomerEntity): CustomerEntity {
-    throw new Error('This method is not implemented');
-  }
-
-  delete(id: string, soft?: boolean): void {
-    throw new Error('This method is not implemented');
-  }
-
-  findAll(): CustomerEntity[] {
-    throw new Error('This method is not implemented');
-  }
-
-  findOneById(id: string): CustomerEntity {
-    throw new Error('This method is not implemented');
-  }
+export class CustomerRepository extends Repository<CustomerEntity>  {
+  
 }
--- src/persistence/repositories/document-type.repository.ts
+++ src/persistence/repositories/document-type.repository.ts
@@ -1,32 +1,8 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from "./base/Repository";
 import { DocumentTypeEntity } from '../entities';
-import { IRepositoriesGlobal } from './interfaces/repositories-global.interface';
 
 @Injectable()
-export class DocumentTypeRepository{
-  private readonly database: Array<DocumentTypeEntity>;//coleccion de documentos
+export class DocumentTypeRepository extends Repository<DocumentTypeEntity>  {
 
-  constructor() {
-    this.database = new Array<DocumentTypeEntity>();
-  }
-
-  register(entity: DocumentTypeEntity): DocumentTypeEntity {
-    throw new Error('This method is not implemented');
-  }
-
-  update(id: string, entity: DocumentTypeEntity): DocumentTypeEntity {
-    throw new Error('This method is not implemented');
-  }
-
-  delete(id: string, soft?: boolean | undefined): void {
-    throw new Error('This method is not implemented');
-  }
-
-  findAll(): DocumentTypeEntity[] {
-    throw new Error('This method is not implemented');
-  }
-
-  findOneById(id: string): DocumentTypeEntity {
-    throw new Error('This method is not implemented');
-  }
 }
