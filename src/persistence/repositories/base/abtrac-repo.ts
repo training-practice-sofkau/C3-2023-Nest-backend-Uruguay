d315a3f0aacdadd82934f7cd76182fb108dcc4b4
brunof <dlqq@outlook.es>
2023-01-25 02:21:02 -0300

    Modificando  implementacion del modelo

--- src/persistence/repositories/base/abtrac-repo.ts
+++ src/persistence/repositories/base/abtrac-repo.ts
@@ -1,6 +1,7 @@
+
 export abstract class AbstracRepo<T> {
 
-    protected readonly database: T[];
+    protected readonly database: T[]; //solo lectura https://www.tutorialsteacher.com/typescript/typescript-readonly
 
     constructor() {
         this.database = new Array<T>();
