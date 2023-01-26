d171eef299553961fcf0b5e143cc7f3d20121834
Rodrigo Caraballo <rodrigomcaraballo8@gmail.com>
2023-01-26 12:34:29 -0300

    Document Type Repository Added
    Adding the repository with all methods finished

--- src/persistence/repositories/document_type.repository.ts
+++ src/persistence/repositories/document_type.repository.ts
@@ -1,26 +1,75 @@
-import { CRUDRepo } from './interfaces/CRUD.interface';
-import { DocumentTypeEntity } from '../entities/document_type.entity';
-export class DocumentTypeRepository implements CRUDRepo {
-    private readonly database: Array<DocumentTypeEntity>;
+import { CRUD } from './base/';
+import { DocumentTypeEntity } from '../entities';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { DocumentTypeRepositoryInterface } from './interfaces/document-type.repository.interface';
 
-    constructor() {
-        this.database = new Array<DocumentTypeEntity>;
-    }
+@Injectable()
+export class DocumentTypeRepository
+  extends CRUD<DocumentTypeEntity>
+  implements DocumentTypeRepositoryInterface
+{
+  register(entity: DocumentTypeEntity): DocumentTypeEntity {
+    const indexCurrentEntity = this.database.findIndex(
+      (item) => item.id === entity.id,
+    );
+    if (indexCurrentEntity != -1) throw new Error('The Document Type already exists');
 
-    register(entity: DocumentTypeEntity): DocumentTypeEntity {
-        throw new Error('Method not implemented.');
-    }
-    update(entity: DocumentTypeEntity): DocumentTypeEntity {
-        throw new Error('Method not implemented.');
-    }
-    delete(entity: DocumentTypeEntity): void {
-        throw new Error('Method not implemented.');
-    }
-    findAll(): DocumentTypeEntity[] {
-        throw new Error('Method not implemented.');
-    }
-    findById(id: string): DocumentTypeEntity {
-        throw new Error('Method not implemented.');
-    }
+    this.database.push(entity);
+    return this.database.at(-1) ?? entity;
+  }
 
+  update(id: string, entity: DocumentTypeEntity): DocumentTypeEntity {
+    const indexCurrentEntity = this.database.findIndex(
+      (item) => item.id === id
+    );
+    if (indexCurrentEntity === -1) throw new NotFoundException();
+
+    this.database[indexCurrentEntity] = {
+      ...this.database[indexCurrentEntity],
+      ...entity,
+      id,
+    } as DocumentTypeEntity;
+
+    return this.database[indexCurrentEntity];
+  }
+
+  delete(id: string, soft?: boolean | undefined): void {
+    const indexCurrentEntity = this.database.findIndex(
+      (item) => item.id === id
+    );
+
+    if (indexCurrentEntity == -1) throw new NotFoundException();
+
+    this.hardDelete(indexCurrentEntity);
+  }
+
+  private hardDelete(index: number): void {
+    this.database.splice(index);
+  }
+
+  findAll(): DocumentTypeEntity[] {
+    return this.database.map(item => item);
+  }
+
+  findOneById(id: string): DocumentTypeEntity {
+    const currentEntity = this.database.find(
+      (item) => item.id === id
+    );
+    if (!currentEntity) throw new NotFoundException();
+
+    return currentEntity;
+  }
+
+  findByState(state: boolean): DocumentTypeEntity[] {
+    return this.database.filter(
+      (item) => item.state === state
+    );
+  }
+
+  findByName(name: string): DocumentTypeEntity[] {
+    return this.database.filter(
+      (item) =>
+        item.name === name
+    );
+  }
 }
