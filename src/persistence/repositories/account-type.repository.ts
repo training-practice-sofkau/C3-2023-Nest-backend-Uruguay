051ed10a4c8b5fac410962f3c90dd9ce54cfe8f1
jhonatan239 <jhonatan239@hotmail.com>
2023-01-26 15:45:27 -0300

    Title: Se finalizan los cambios en customerRepository
    se finaliza los metodos de customer y ahora se verifican y se comienzan a implmentar las funciones similares en los demas repository

--- src/persistence/repositories/account-type.repository.ts
+++ src/persistence/repositories/account-type.repository.ts
@@ -1,55 +1,55 @@
 import { Injectable } from '@nestjs/common';
-import { basename } from 'path';
-import { DocumentTypeEntity } from '../entities';
 import { Base } from './base/base.abstract';
 import { CRUD } from './interfaces/crud.interface';
-import { AccountEntity } from '../entities/account.entity';
 import { AccountTypeEntity } from '../entities/account-type.entity';
 
 
 @Injectable()
 export class AccountTypeRepository extends Base<AccountTypeEntity> implements CRUD<AccountTypeEntity>{
-
-
+  
+  
   register(entity: AccountTypeEntity): AccountTypeEntity {
     this.database.push(entity);
     return this.database.at(-1) ?? entity;
-  }
+}
 
-  update(id: string, entity: AccountTypeEntity): AccountTypeEntity {
-    const indexCurrentEntity = this.database.findIndex(
-      (item) => item.id === id,
-    );
-    if (indexCurrentEntity >= 0)
-      this.database[indexCurrentEntity] = {
-        ...this.database[indexCurrentEntity],
-        ...entity,
-        id,
-      } as DocumentTypeEntity;
-    else throw new DocumentTypeEntity();
-    return this.database[indexCurrentEntity];
-  }
-  }
+update(id: string, entity: AccountTypeEntity):AccountTypeEntity {
+  const indexCurrentEntity = this.database.findIndex(
+    (item) => item.id === id,
+  );
+  if (indexCurrentEntity >= 0)
+    this.database[indexCurrentEntity] = {
+      ...this.database[indexCurrentEntity],
+      ...entity,
+      id,
+    } as AccountTypeEntity;
+  else throw new AccountTypeEntity();
+  return this.database[indexCurrentEntity];
+}
 
-  delete(id: string, soft?: boolean | undefined): void {
+delete(id: string, soft?: boolean | undefined): void {
     throw new Error('Method not implemented.');
-  }
+}
 
-  findAll(): AccountTypeEntity[] {
+findAll(): AccountTypeEntity[] {
+  if (this.database.length == 0) {
+    throw new Error('No se encontraron elementos');
+    }
+    return this.database
+   
+}
+
+findOneById(id: string): AccountTypeEntity {
     throw new Error('Method not implemented.');
-  }
+}
 
-  findOneById(id: string): AccountTypeEntity{
-    throw new Error('Method not implemented.');
-  }
-
-  findByState(state: boolean): AccountTypeEntity[] {
+findByState(state: boolean): AccountTypeEntity[] {
     throw new Error('This method is not implemented');
-  }
+}
 
-  findByName(name: string): AccountTypeEntity[] {
+findByName(name: string): AccountTypeEntity[] {
     throw new Error('This method is not implemented');
-  }
+}
 
 
 }
