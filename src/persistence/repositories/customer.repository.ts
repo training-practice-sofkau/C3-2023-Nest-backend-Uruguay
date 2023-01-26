6ef4a2743c76ee4fe11ae330babed8ff057f8b59
herweg <martindelgado2011@gmail.com>
2023-01-26 16:10:28 -0300

    Customer Repository

--- src/persistence/repositories/customer.repository.ts
+++ src/persistence/repositories/customer.repository.ts
@@ -5,76 +5,103 @@ import { CustomerRepositoryInterface } from './interfaces';
 
 @Injectable()
 export class CustomerRepository
-    extends BaseRepository<CustomerEntity>
-    implements CustomerRepositoryInterface {
+  extends BaseRepository<CustomerEntity>
+  implements CustomerRepositoryInterface {
 
-        register(entity: CustomerEntity): CustomerEntity {
-            this.database.push(entity);
-            return this.database.at(-1) ?? entity;
-          }
-        
-          update(id: string, entity: CustomerEntity): CustomerEntity {
-            const indexCurrentEntity = this.database.findIndex(
-              (item) => item.id === id && typeof item.deletedAt === 'undefined',
-            );
-            if (indexCurrentEntity >= 0)
-              this.database[indexCurrentEntity] = {
-                ...this.database[indexCurrentEntity],
-                ...entity,
-                id,
-              } as CustomerEntity;
-            else throw new NotFoundException();
-            return this.database[indexCurrentEntity];
-          }
-        
-          delete(id: string, soft?: boolean | undefined): void {
-            throw new Error('Method not implemented.');
-          }
-        
-          findAll(): CustomerEntity[] {
-            return this.database.filter(
-              (item) => typeof item.deletedAt === 'undefined',
-            );
-          }
-        
-          findOneById(id: string): CustomerEntity {
-            const currentEntity = this.database.find(
-              (item) => item.id === id && typeof item.deletedAt === 'undefined',
-            );
-            if (currentEntity) return currentEntity;
-            else throw new NotFoundException();
-          }
-        
-          findOneByEmailAndPassword(email: string, password: string): boolean {
-            const indexCurrentEntity = this.database.findIndex(
-              (item) =>
-                item.email === email &&
-                item.password === password &&
-                typeof item.deletedAt === 'undefined',
-            );
-            return indexCurrentEntity >= -1 ? true : false;
-          }
-        
-          findOneByDocumentTypeAndDocument(
-            documentTypeId: string,
-            document: string,
-          ): CustomerEntity {
-            throw new Error('This method is not implemented');
-          }
-        
-          findOneByEmail(email: string): CustomerEntity {
-            throw new Error('This method is not implemented');
-          }
-        
-          findOneByPhone(phone: string): CustomerEntity {
-            throw new Error('This method is not implemented');
-          }
-        
-          findByState(state: boolean): CustomerEntity[] {
-            throw new Error('This method is not implemented');
-          }
-        
-          findByFullName(fullName: string): CustomerEntity[] {
-            throw new Error('This method is not implemented');
-          }
+  register(entity: CustomerEntity): CustomerEntity {
+    this.database.push(entity);
+    return this.database.at(-1) ?? entity;
+  }
+
+  update(id: string, entity: CustomerEntity): CustomerEntity {
+    const indexCurrentEntity = this.database.findIndex(
+      (item) => item.id === id
+        && typeof item.deletedAt === 'undefined',
+    )
+    if (indexCurrentEntity >= 0)
+      this.database[indexCurrentEntity] = {
+        ...this.database[indexCurrentEntity],
+        ...entity,
+        id,
+      } as CustomerEntity;
+    else throw new NotFoundException();
+    return this.database[indexCurrentEntity];
+  }
+
+  delete(id: string, soft?: boolean | undefined): void {
+    throw new Error('Method not implemented.');
+  }
+
+  findAll(): CustomerEntity[] {
+    return this.database.filter(
+      (item) => typeof item.deletedAt === 'undefined',
+    );
+  }
+
+  findOneById(id: string): CustomerEntity {
+    const currentEntity = this.database.find(
+      (item) => item.id === id
+        && typeof item.deletedAt === 'undefined',
+    );
+    if (currentEntity) return currentEntity;
+    else throw new NotFoundException();
+  }
+
+  findOneByEmailAndPassword(email: string, password: string): boolean {
+    const indexCurrentEntity = this.database.findIndex(
+      (item) => item.email === email
+        && item.password === password
+        && typeof item.deletedAt === 'undefined',
+    );
+    return indexCurrentEntity >= -1 ? true : false;
+  }
+
+  findOneByDocumentTypeAndDocument(
+    documentTypeId: string,
+    document: string,
+  ): CustomerEntity {
+    const currentEntity = this.database.find(
+      (item) => item.documentType.id === documentTypeId
+        && item.document === document
+        && typeof item.deletedAt === 'undefined',
+    );
+    if (!currentEntity) throw new NotFoundException()
+    else return currentEntity
+  }
+
+  findOneByEmail(email: string): CustomerEntity {
+    const currentEntity = this.database.find(
+      (item) => item.email === email
+        && typeof item.deletedAt === 'undefined',
+    );
+    if (!currentEntity) throw new NotFoundException()
+    else return currentEntity
+  }
+
+  findOneByPhone(phone: string): CustomerEntity {
+    const currentEntity = this.database.find(
+      (item) => item.phone === phone
+        && typeof item.deletedAt === 'undefined',
+    );
+    if (!currentEntity) throw new NotFoundException()
+    else return currentEntity
+  }
+
+  findByState(state: boolean): CustomerEntity[] {
+    const currentEntity = this.database.filter(
+      (item) => item.state === state
+        && typeof item.deletedAt === 'undefined',
+    )
+    if (!currentEntity) throw new NotFoundException()
+    else return currentEntity
+  }
+
+  findByFullName(fullName: string): CustomerEntity[] {
+    const currentEntity = this.database.filter(
+      (item) => item.fullName === fullName
+        && typeof item.deletedAt === 'undefined',
+    )
+    if (!currentEntity) throw new NotFoundException()
+    else return currentEntity
+  }
 }
