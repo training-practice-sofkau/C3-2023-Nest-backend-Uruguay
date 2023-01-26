5d347a2f215a571ca1ba2340227ababbae138d6e
herweg <martindelgado2011@gmail.com>
2023-01-26 14:31:06 -0300

    2.30

--- src/persistence/repositories/account-type.repository.ts
+++ src/persistence/repositories/account-type.repository.ts
@@ -1,23 +1,39 @@
 import { Injectable } from "@nestjs/common";
 import { AccountTypeEntity } from "../entities/account-type.entity";
-import { Repository } from "./base/repository.base";
-import { IRepository } from './interfaces/repository.interface';
+import { BaseRepository } from './base/base.repository';
+import { AccountTypeRepositoryInterface } from "./interfaces";
 
 @Injectable()
-export class AccountTypeRepository extends Repository<AccountTypeEntity> implements IRepository<AccountTypeEntity>{
-    register(entity: AccountTypeEntity): AccountTypeEntity {
-        throw new Error("Method not implemented.");
-    }
-    update(id: string, entity: AccountTypeEntity): AccountTypeEntity {
-        throw new Error("Method not implemented.");
-    }
-    delete(id: string, soft?: boolean | undefined): void {
-        throw new Error("Method not implemented.");
-    }
-    findAll(): AccountTypeEntity[] {
-        throw new Error("Method not implemented.");
-    }
-    findOneById(id: string): AccountTypeEntity {
-        throw new Error("Method not implemented.");
-    }
+export class AccountTypeRepository
+    extends BaseRepository<AccountTypeEntity>
+    implements AccountTypeRepositoryInterface {
+        
+        register(entity: AccountTypeEntity): AccountTypeEntity {
+            this.database.push(entity)
+            return this.database.at(-1) ?? entity
+        }
+    
+        update(id: string, entity: AccountTypeEntity): AccountTypeEntity  {
+            throw new Error('Method not implemented.');
+        }
+    
+        delete(id: string, soft?: boolean | undefined): void {
+            throw new Error('Method not implemented.');
+        }
+    
+        findAll(): AccountTypeEntity[] {
+            throw new Error('Method not implemented.');
+        }
+    
+        findOneById(id: string): AccountTypeEntity  {
+            throw new Error('Method not implemented.');
+        }
+    
+        findByState(state: boolean): AccountTypeEntity[] {
+            throw new Error('This method is not implemented');
+        }
+    
+        findByName(name: string): AccountTypeEntity[] {
+            throw new Error('This method is not implemented');
+        }
 }
