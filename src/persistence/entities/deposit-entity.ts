8cd6c22008c73fd92fc13beb6480a7a9d8cd057a
brunof <dlqq@outlook.es>
2023-01-26 21:26:55 -0300

    Commit findByAccountId

--- src/persistence/entities/deposit-entity.ts
+++ src/persistence/entities/deposit-entity.ts
@@ -4,6 +4,7 @@ import { IDepositModel } from "src/models/i-deposit-model";
 import { v4 as uuid } from 'uuid';
 
 export class DepositEntity implements IDepositModel{
+    [x: string]: any;
     
     id = uuid();
     account: IAccountModel;
