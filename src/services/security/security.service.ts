0280217d0d78d2b53b844ab772c4c4da1cdbb6ad
brunof <dlqq@outlook.es>
2023-01-27 21:41:03 -0300

    Commit SecurityService

--- src/services/security/security.service.ts
+++ src/services/security/security.service.ts
@@ -7,75 +7,71 @@ import { AccountService } from '../account';
 import { ICustomerModel } from 'src/models/i-customer-model';
 import { Response } from 'express';
 import  jwt from 'jsonwebtoken';
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
 export class SecurityService {
     constructor(private readonly customerRepository: CustomerRepo,
                 private readonly accountService: AccountService) {}
     
+     
       /**
-       * Identificarse en el sistema
-       *
-       * @param {CustomerModel} user
-       * @return {*}  {string}
-       * @memberof SecurityService
-       */
-      signIn(user: ICustomerModel): string {
-        const answer = this.customerRepository.findOneByEmailAndPassword(
-          user.email,
-          user.password,
-        );
-
+     * Identificarse en el sistema
+     *
+     * @param {CustomerModel} user
+     * @return {*}  {string}
+     * @memberof SecurityService
+     */
+    signIn(user: ICustomerModel): string {
+      const answer = this.customerRepository.findOneByEmailAndPassword(
+        user.email,
+        user.password,
+      );
+      if (answer) return jwt.sign(user, process.env.TOKEN_SECRET || "tokentest");
+      else throw new UnauthorizedException();
+    }
+  
+    /**
+     * Crear usuario en el sistema
+     *
+     * @param {CustomerModel} user
+     * @return {*}  {string}
+     * @memberof SecurityService
+     */
+    signUp(user: ICustomerModel): string {
+      const newCustomer = new CustomerEntity();
+      newCustomer.documentType = user.documentType;
+      newCustomer.document = user.document;
+      newCustomer.fullName = user.fullName;
+      newCustomer.email = user.email;
+      newCustomer.phone = user.phone;
+      newCustomer.password = user.password;
+  
+      const customer = this.customerRepository.register(newCustomer);
+  
+      if (customer) {
+        const accountType = new AccountEntity();
+        accountType.id = uuid();
+        const newAccount = {
+          ...customer,
+          ...accountType,
+        };
+        const account = this.accountService.createAccount(newAccount);
         
-        if (!answer) throw new UnauthorizedException();
+        if (account) return jwt.sign(user, process.env.TOKEN_SECRET || "tokentest");
+        else throw new InternalServerErrorException();
+      } else throw new InternalServerErrorException();
+    }
+  
+    /**
+     * Salir del sistema
+     *
+     * @param {string} JWToken
+     * @memberof SecurityService
+     */
+    signOut(JWToken: string): void {
 
-        return jwt.sign({id: user.id}, process.env.TOKEN_SECRET || 'tokentest');
-      }
-    
-      /**
-       * Crear usuario en el sistema
-       *
-       * @param {CustomerModel} user
-       * @return {*}  {string}
-       * @memberof SecurityService
-       */
-      signUp(user: ICustomerModel): string {
-        const newCustomer = new CustomerEntity();
-        newCustomer.documentType = user.documentType;
-        newCustomer.document = user.document;
-        newCustomer.fullName = user.fullName;
-        newCustomer.email = user.email;
-        newCustomer.phone = user.phone;
-        newCustomer.password = user.password;
-    
-        const customer = this.customerRepository.register(newCustomer);
-    
-        if (customer) {
-          const accountType = new AccountTypeEntity();
-          accountType.id = 'Falta el ID por defecto del tipo de cuenta';
-          let newAccount = new AccountEntity();
-          newAccount = {
-            ...newAccount,
-            customer,
-            accountType,
-          };
-    
-          const account = this.accountService.createAccount(newAccount);
-    
-          if (account) throw new InternalServerErrorException();
+    }
+  }
 
-          return jwt.sign({id: user.id}, process.env.TOKEN_SECRET || 'tokentest');
-        } else throw new InternalServerErrorException();
-      }
-    
-      /**
-       * Salir del sistema
-       *
-       * @param {string} JWToken
-       * @memberof SecurityService
-       */
-      signOut(JWToken: string, res: Response): void {
-        res.clearCookie(JWToken);
-      }
-}
 
