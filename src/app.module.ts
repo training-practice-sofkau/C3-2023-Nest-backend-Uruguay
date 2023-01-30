b67c284a29d399ba085e8081297f6a5ec292facb
Rodrigo Caraballo <rodrigomcaraballo8@gmail.com>
2023-01-30 13:28:30 -0300

    Update in projects files
    Updating json files, main and module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,20 @@
 import { Module } from '@nestjs/common';
 import { SecurityController } from './controllers';
 import { AccountService } from './services';
-import { AccountTypeService } from './services/account_type/';
 import { CustomerService } from './services/customer/';
 import { DepositService } from './services/deposit/';
-import { DocumentTypeService } from './services/document_type/';
 import { TransferService } from './services/transfer/';
+import { AccountRepository } from './persistence/repositories/account.repository';
+import { CustomerRepository } from './persistence/repositories/customer.repository';
+import { DepositRepository } from './persistence';
+import { TransferRepository } from './persistence/repositories/transfer.repository';
+import { AccountTypeRepository } from './persistence/repositories/account-type.repository';
+import { DocumentTypeRepository } from './persistence/repositories/document-type.repository';
+import { SecurityService } from './services/security/security.service';
 
 @Module({
   imports: [],
   controllers: [SecurityController],
-  providers: [AccountService, AccountTypeService, CustomerService, DepositService, DocumentTypeService, TransferService],
+  providers: [AccountService, CustomerService, DepositService, TransferService, SecurityService, AccountRepository, CustomerRepository, DepositRepository, TransferRepository, AccountTypeRepository, DocumentTypeRepository],
 })
 export class AppModule {}
