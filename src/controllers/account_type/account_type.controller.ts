cbfdc1b3e346f012ca3556e45bc81ec4f45406db
Rodrigo Caraballo <rodrigomcaraballo8@gmail.com>
2023-01-30 13:25:10 -0300

    Modify Controller Folder
    Removing some files and Updating some others

--- src/controllers/account_type/account_type.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('account-type')
-export class AccountTypeController {}
--- src/controllers/customer/customer.controller.ts
+++ src/controllers/customer/customer.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Param, Post, Put, Delete } from '@nestjs/common';
+import { CustomerService } from '../../services/customer/customer.service';
+import { CreateCustomerDTO } from '../../dtos/create-customer.dto';
+import { CustomerEntity } from '../../persistence/entities/customer.entity';
+import { PaginationModel } from '../../../dist/models/pagination-model.model';
+import { UpdateCustomerDTO } from '../../dtos/update-customer.dto';
 
 @Controller('customer')
-export class CustomerController {}
+export class CustomerController {
+
+    constructor(private readonly customerService: CustomerService) {}
+
+    @Post()
+    createCustomer(customer: CreateCustomerDTO): CustomerEntity {
+        return this.customerService.createCustomer(customer);
+    }
+
+    @Put('/update/:id')
+    updateCustomer(@Param() id: string ,@Body() newCustomer: UpdateCustomerDTO): CustomerEntity {
+        return this.customerService.updatedCustomer(id, newCustomer);
+    }
+
+    @Delete('/soft-delete/:id')
+    unsuscribe(id: string): void {
+        this.customerService.deleteCustomer(id);
+    }
+    @Delete('/hard-delete/:id')
+    hardDelete(id: string): void {
+        this.customerService.deleteCustomer(id);
+    }
+}
