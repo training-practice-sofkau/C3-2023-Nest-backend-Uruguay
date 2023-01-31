20b2a0e5c05b3f751c2cd2827a6a79e5d804504d
Rodrigo Caraballo <rodrigomcaraballo8@gmail.com>
2023-01-31 17:05:13 -0300

    DTOS
    Adding decorators to some dtos

--- src/business/dtos/customer-state.dto.ts
+++ src/business/dtos/customer-state.dto.ts
@@ -1,3 +1,6 @@
+import { IsBoolean } from "class-validator";
+
 export class CustomerStateDTO {
+    @IsBoolean()
     state: boolean;
 }
--- src/business/dtos/sing-out.dto.ts
+++ src/business/dtos/sing-out.dto.ts
@@ -1,3 +1,6 @@
+import { IsString } from "class-validator";
+
 export class SingOutDTO {
+    @IsString()
     token: string;
 }
