3e2e4c464e1af625c74bf43d5d4f37467eb0e250
matisouza <matias.souza@sofka.com.co>
2023-02-10 14:13:58 -0300

    CORS

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,11 @@ import { NestFactory } from '@nestjs/core';
 
 // Main module
 import { AppModule } from './app.module';
-import { AccountTypeEntity, DocumentTypeEntity } from './data/persistence/entities/';
-import { AccountTypeRepository, DocumentTypeRepository } from './data/persistence/repositories/';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  // app.useGlobalPipes(new ValidationPipe({
-  //   whitelist: true,
-  //   errorHttpStatusCode: 406
-  // }
-  // ))
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.enableCors();
+
   await app.listen(3000);
 }
 bootstrap();
